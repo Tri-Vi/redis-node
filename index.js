e5686cab261f42c18cce839633bd07b77893955a
Tri-Vi <36706183+Tri-Vi@users.noreply.github.com>
2023-02-17 15:50:55 -0600

    Update server file
    - Install dotenv and redis library
    - Create api route and index route
    - Import redis library to main server file
    - Update gitignore file

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000
-
-
+const redis = require('redis');
+const redisClient = redis.createClient(process.env.REDIS_PORT);
+redisClient.on('error', (err)=>{
+  console.error(err.message);
+})
 app.get('/', (req, res) =>{
   res.send('<h1>Hello World</h1>')
-})
+});
+
+
+
 app.listen(port, (req,res) => {
   console.log(`listening on ${port}`)
 })
