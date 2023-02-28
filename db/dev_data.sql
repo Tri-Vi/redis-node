fb9e8e432c7c47a86b276578527f3ee03c9d9337
Tri-Vi <36706183+Tri-Vi@users.noreply.github.com>
2023-02-28 11:05:18 -0600

    Create dev_data.sql
    - Create dummy data for dev

---
+++ db/dev_data.sql
@@ -0,0 +1,9 @@
+-- BOOK
+INSERT INTO `Book` (`id`, `name`) VALUES (1, 'Bank 4.0');
+INSERT INTO `Book` (`id`, `name`) VALUES (2, 'JavaScript');
+INSERT INTO `Book` (`id`, `name`) VALUES (3, 'System Design Interview');
+INSERT INTO `Book` (`id`, `name`) VALUES (4, 'Coding Interview');
+INSERT INTO `Book` (`id`, `name`) VALUES (5, 'Python 3');
+ 
+ -- USER
+ INSERT INTO `User` (`id`, `email`, `first_name`, `last_name`) VALUES (1, 'jdoe01@gmail.com', 'John', 'Doe');
