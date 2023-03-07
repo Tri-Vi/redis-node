87694550df593c9c5eb654b3ff1c4fbd3bff4604
Tri-Vi <36706183+Tri-Vi@users.noreply.github.com>
2023-03-07 17:28:02 -0600

    Render user and Book info on table
    - Render User Info and Book Info on Table
    - Create new BookProfile and UserProfile views

--- redis-node-client/src/components/UserTable.vue
+++ redis-node-client/src/components/UserTable.vue
@@ -1,18 +1,48 @@
 <template>
   <div>
-    <h3>Users Dashboard</h3>
-    <div class="users">
-
-      <div v-for="user in allUsers" :key="user.id" class="user">
-        {{user.id}} | {{ user.first_name }} | {{user.last_name}} |  {{user.email}}
-      </div>
-    </div>
+    <v-data-table
+      :headers="headers"
+      :items="allUsers"
+      :items-per-page="5"
+      class="elevation-1"
+    ></v-data-table>
   </div>
 </template>
 
 <script>
 import  { mapGetters, mapActions } from 'vuex';
 export default {
+  data(){
+    return {
+      headers: [
+        {
+          text: 'Id',
+          sortable: true,
+          value: 'id'
+        },
+        {
+          text: 'First Name',
+          sortable: true,
+          value: 'id'
+        },
+        {
+          text: 'Last Name',
+          sortable: true,
+          value: 'id'
+        },
+        {
+          text: 'Email',
+          sortable: true,
+          value: 'id'
+        },
+        {
+          text: 'Action',
+          sortable: false,
+          value: ''
+        },
+      ]
+    }
+  },
   methods: {
     ...mapActions(['fetchUsers'])
   },
