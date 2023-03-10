38140e9dc005c4a7e06be4a645ff79b1a938d1a1
Tri-Vi <36706183+Tri-Vi@users.noreply.github.com>
2023-03-10 12:57:22 -0600

    MOD
    #9
    - Update BookTable
    - Fix calling wrong db model in Book controller
    - Update book store

--- redis-node-client/src/store/modules/books.js
+++ redis-node-client/src/store/modules/books.js
@@ -1,20 +1,43 @@
 import axios from 'axios';
 
 const state = {
-  books: []
+  books: [],
+  errors: [],
 }
 
 const getters = {
-  allBooks: state => state.books
+  allBooks: state => state.books,
+  errors: state => state.errors
 }
 const actions = {
   async fetchBooks({commit}){
-    const response = await axios.get('http://localhost:3000/api/book');
-    commit('setBooks', response.data.rows)
+    try {
+      const response = await axios.get('http://localhost:3000/api/book');
+      commit('setBooks', response.data.rows)
+    } catch(error){
+      commit('setErrors', error);
+    }
+  },
+  async addBook({commit}, payload){
+    try {
+      const response = await axios.post('http://localhost:3000/api/book', payload);
+      commit('newBook', response.data)
+    } catch(error){
+      commit('setErrors', error)
+    }
+  },
+  async editBook({commit}, payload){
+    const response = await axios.put('http://localhost:3000/api/book', payload);
+    commit('testMutation', response)
   }
 }
 const mutations = {
-  setBooks: (state, books) => {state.books = books}
+  setBooks: (state, books) => {state.books = books},
+  setErrors: (state, error) => {state.errors.push(error)},
+  testMutation: (state, payload) => {console.log(payload)},
+  newBook: (state, book) => { 
+    state.books.push(book)
+  }
 }
 export default {
   state,
