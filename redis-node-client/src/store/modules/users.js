174a99450fcbe918c6805b5edc8fd1b4488dd587
Tri-Vi <36706183+Tri-Vi@users.noreply.github.com>
2023-03-10 14:31:06 -0600

    Update users.js
    #9
    - Update user store's actions and mutations

--- redis-node-client/src/store/modules/users.js
+++ redis-node-client/src/store/modules/users.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const state = {
-  users: []
+  users: [],
+  errors: []
 };
 
 const getters = {
@@ -12,10 +13,49 @@ const actions = {
     const response = await axios.get('http://localhost:3000/api/user');
     commit('setUsers', response.data.rows)
   },
+  async addUser({commit}, payload){
+    try {
+      const response = await axios.post('http://localhost:3000/api/user', payload);
+      commit('newUser', response.data)
+    } catch(error){
+      commit('setErrors', error)
+    }
+  },
+  async editBook({commit}, payload){
+    try {
+      const response = await axios.put(`http://localhost:3000/api/user/${payload.editedItem.id}`, payload.editedItem);
+      commit('editUser', {
+        editedIndex: payload.editedIndex,
+        ...response
+      })
+    } catch(error){
+      commit('setErrors', error);
+    }
+  },
+  async deleteBook({commit}, payload){
+    try {
+      await axios.delete(`http://localhost:3000/api/user/${payload.editedItem.id}`)
+      commit('deleteUser', {
+        editedIndex: payload.editedIndex,
+      })
+    } catch(error){
+      commit('setErrors', error);
+    }
+  }
 };
 
 const mutations = {
-  setUsers: (state, users) => (state.users = users) 
+  setUsers: (state, users) => (state.users = users),
+  setErrors: (state, error) => {state.errors.push(error)},
+  newUser: (state, user) => { 
+    state.users.push(user)
+  },
+  editBook: (state, payload) => {
+    Object.assign(state.users[payload.editedIndex], payload.data);
+  },
+  deleteBook: (state, payload) => {
+    state.users.splice(payload.editedIndex, 1)
+  } 
 };
 
 export default {
