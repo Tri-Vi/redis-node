130bff77562cb240eb72e7de2aeb67712f304b2a
Tri-Vi <36706183+Tri-Vi@users.noreply.github.com>
2023-05-23 17:37:11 -0500

    Multiple Updates
    #19
    - Update API Test Suite
    - Update nginx config

--- redis-node-client/cypress/e2e/user_api.cy.js
+++ redis-node-client/cypress/e2e/user_api.cy.js
@@ -1,10 +1,32 @@
-// Get User
-describe('API tests', () => {
-  it('should make a successful API request', () => {
-    cy.request('GET', 'http://localhost:8080/api/book')
-      .then((response) => {
+// USER - API TEST SUITE
+
+describe('User API tests', ()=> {
+  it('should make a successful get request', ()=> {
+    cy.request('GET', 'http://localhost:8080/api/user')
+      .then((response)=>{
         expect(response.status).to.eq(200);
-        //expect(response.body).to.have.property('data');
+        expect(response.body).to.have.property('rows');
+        expect(response.body.rows).to.be.an('array').and.to.have.length.gte(0);
       });
   });
-});
+
+  it('should add a user', ()=>{
+    const requestBody = {
+      first_name: "FIRST NAME - CYPRESS TESTING",
+      last_name: "LAST NAME - CYPRESS TESTING",
+      email: "EMAIL - CYPRESS TESTING"
+    }
+    cy.request('POST', 'http://localhost:8080/api/user', requestBody)
+      .then((response) => {
+        expect(response.status).to.eq(200);
+        expect(response.body).to.have.property('id');
+        expect(response.body).to.have.property('first_name');
+        expect(response.body).to.have.property('last_name');
+        expect(response.body).to.have.property('email');
+        expect(response.body.first_name).to.eq(requestBody.first_name);
+        expect(response.body.last_name).to.eq(requestBody.last_name);
+        expect(response.body.email).to.eq(requestBody.email);
+
+      })
+  });
+})
