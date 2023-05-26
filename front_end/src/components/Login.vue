5501842d45ab3166f297b8d4d226a111a4c1f5e2
Tri-Vi <36706183+Tri-Vi@users.noreply.github.com>
2023-05-26 10:47:22 -0500

    Create Login and Register Components
    #14
    - Create Login and Register Components

---
+++ front_end/src/components/Login.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <input v-model="username" type="text" placeholder="Username"/>
+    <input v-model="password" type="password" placeholder="Password" /> 
+    <button @click="login"><button>
+  </div>
+</template>
+<script>
+import {mapActions} from vuex;
+
+export default {
+  data(){
+    return {
+      username: '',
+      password: ''
+    };
+  },
+  methods: {
+    ...mapActions(['login']),
+    async login() {
+      const { success, error } = await this.login({
+        username: this.username,
+        password: this.password
+      });
+
+      if(success){
+        // Redirect or perform any necessary action after successful login
+      } else {
+        console.log('Login failed:' , error);
+      }
+    }
+  }
+}
+</script>
---
+++ front_end/src/components/Register.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <input v-model="username" type="text" placeholder="Username" />
+    <input v-model="password" type="password" placeholder="Password" />
+    <button @click="register">Register</button>
+  </div>
+</template>
+
+<script>
+import { mapActions } from 'vuex';
+
+export default {
+  data() {
+    return {
+      username: '',
+      password: ''
+    };
+  },
+  methods: {
+    ...mapActions(['register']),
+    async register() {
+      const { success, error } = await this.register({
+        username: this.username,
+        password: this.password
+      });
+      if (success) {
+        // Redirect or perform any necessary action after successful registration
+      } else {
+        console.log('Registration failed:', error);
+      }
+    }
+  }
+};
+</script>
